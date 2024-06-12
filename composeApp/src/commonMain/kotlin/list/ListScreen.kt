54a7b14f004537d13ace200b688a80e7326f80ee
silverstar <tjddmsdks@gmail.com>
2024-06-13 03:21:44 +0900

    List 화면 UI 수정

--- composeApp/src/commonMain/kotlin/list/ListScreen.kt
+++ composeApp/src/commonMain/kotlin/list/ListScreen.kt
@@ -1,11 +1,16 @@
 package list
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import root.component.DefaultRootComponent
 
@@ -14,13 +19,19 @@ fun ListScreen(
     component: ListComponent,
     navigateTo: (DefaultRootComponent.Config) -> Unit,
 ) {
-    Column(modifier = Modifier.fillMaxSize()) {
-        Text(
-            text = "go to cubic!",
-            fontSize = 24.sp,
-            modifier = Modifier.clickable {
-                navigateTo(DefaultRootComponent.Config.Cubic)
-            }
-        )
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        item {
+            Text(
+                text = "1. Cubic Background",
+                fontSize = 24.sp,
+                modifier = Modifier
+                    .background(Color.Green)
+                    .fillMaxWidth()
+                    .clickable {
+                        navigateTo(DefaultRootComponent.Config.Cubic)
+                    }
+                    .padding(16.dp)
+            )
+        }
     }
 }
